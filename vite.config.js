ae23c07d4a006b32a867371023f2dfad12135c4e
xXelghaleXx <adriannuno56@gmail.com>
2025-10-24 01:41:47 -0500

    Re-ajustando el frontend para el despliegue #2

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://scancvai-3-0-back.onrender.com:3000',
         changeOrigin: true,
         secure: false,
         configure: (proxy, options) => {
